038037e5ec892f7006e354ec04a17fd10e86c87f
Daisuke Hirata <daisukihirata@gmail.com>
2018-05-08 09:32:33 +0900

    get all acronyms

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -31,6 +31,10 @@ public func routes(_ router: Router) throws {
             return acronym.save(on: req)
         }
     }
+    
+    router.get("api", "acronyms") { req -> Future<[Acronym]> in
+        return Acronym.query(on: req).all()
+    }
 
 }
 
