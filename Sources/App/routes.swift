282f784041d0257d3d5a2ecb05de0f4d7767b133
Daisuke Hirata <daisukihirata@gmail.com>
2018-05-14 09:32:01 +0900

    get first acronym

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -59,7 +59,17 @@ public func routes(_ router: Router) throws {
         guard let searchTerm = req.query[String.self, at: "term"] else {
             throw Abort(.badRequest)
         }
-        return try Acronym.query(on: req).filter(\.short == searchTerm).all()
+        return try Acronym.query(on: req).group(.or) { or in
+            try or.filter(\.short == searchTerm)
+            try or.filter(\.long == searchTerm)
+        }.all()
+    }
+    
+    router.get("api", "acronyms", "first") { req -> Future<Acronym> in
+        return Acronym.query(on: req).first().map(to: Acronym.self) { acronym in
+            guard let acronym = acronym else { throw Abort(.notFound) }
+            return acronym
+        }
     }
 }
 
